74f64b4a039cd6b1ce108145a0282e88294c2ebd
b03505031 <b03505031@ntu.edu.tw>
2018-10-26 17:44:03 +0800

    b304

---
+++ src/b304.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stack.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int nn=0;nn<n;n++){
+        string tocheck;
+        cin>>tocheck;
+        for(int l=0;l<tocheck.length();l++){
+            
+        }
+    }
+}

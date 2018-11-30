38c8fac8f9b7e6e3f0e2973740673c5c8af17180
cilegann <b03505031@ntu.edu.tw>
2018-12-01 02:01:44 +0800

    c

---
+++ src/a686.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int ts;
+    cin>>ts;
+    for(int t=0;t<ts;t++){
+        int x,y,z;
+        cin>>x>>y>>z;
+        if(z<=y){
+            cout<<"Poor Snail"<<endl;
+        }else{
+            cout<<(x-y)/(y-z)+1<<endl;
+        }
+    }
+    return 0;
+}

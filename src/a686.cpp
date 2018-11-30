203bd17b73efdab1472fcce7af4e246d101dadd0
cilegann <b03505031@ntu.edu.tw>
2018-12-01 02:01:58 +0800

    c

--- src/a686.cpp
+++ src/a686.cpp
@@ -7,7 +7,7 @@ int main()
     for(int t=0;t<ts;t++){
         int x,y,z;
         cin>>x>>y>>z;
-        if(z<=y){
+        if(z>=y){
             cout<<"Poor Snail"<<endl;
         }else{
             cout<<(x-y)/(y-z)+1<<endl;

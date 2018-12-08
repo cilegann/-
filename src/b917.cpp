c0e3c9a9d31250b4719d2e8e4c20888ad07f07f8
cilegann <b03505031@ntu.edu.tw>
2018-12-08 14:53:54 +0800

    b917 done

---
+++ src/b917.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int n;
+long long arr[20],ans;
+
+void dfs(int now,long long product){
+    if(product>ans) ans=product;
+    if(now==n)  return;
+    dfs(now+1,product*arr[now]);    
+}
+
+int main(){
+    int c=0;
+    while(cin>>n){
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        ans=0;
+        for(int i=0;i<n;i++){
+            dfs(i+1,arr[i]);
+        }
+        cout << "Case #" << ++c << ": The maximum product is " << ans << "." << endl;
+        cout << endl;
+    }
+
+
+}
